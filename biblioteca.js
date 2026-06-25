6aa7ff6e5168655c84cc1e47695f0656f95b7db9
larissa.melo <larissa.melo@aluno.edu.pi.gov.br>
2026-06-25 16:46:31 +0000

    atividade 25/06 , biblioteca

---
+++ biblioteca.js
@@ -0,0 +1,17 @@
+const biblioteca ={
+    prateleira : [],// array (lista)
+    //1.adicionar prateleira.
+    adicionarlivro (nome,autor,categoria,estoque) {
+        const livros={
+            nome:nome,
+            autor:autor,
+            categoria:categoria,
+            estoque:estoque,
+            disponivel:true,
+            alugado:false,
+            vezesalugado:0 };//objeto
+            this.livros.push(produtos);
+            console.log("//livros adicionado//)livros.nome")
+            },//funcao};livros//biblioteca
+            
+    }
