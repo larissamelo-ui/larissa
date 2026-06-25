dc84d4980771adc57cf5e3c807782c656464c346
larissa.melo <larissa.melo@aluno.edu.pi.gov.br>
2026-06-25 18:05:46 +0000

    enviado

--- biblioteca.js
+++ biblioteca.js
@@ -1,16 +1,34 @@
-const biblioteca ={
-    prateleira : [],// array (lista)
-    //1.adicionar prateleira.
-    adicionarlivro (nome,autor,categoria,estoque) {
-        const livros={
-            nome:nome,
-            autor:autor,
-            categoria:categoria,
-            estoque:estoque,
-            disponivel:true,
-            alugado:false,
-            vezesalugado:0 };//objeto
-            this.livros.push(produtos);
-            console.log("//livros adicionado//)livros.nome")
-            },//funcao};livros//biblioteca
+const biblioteca = {
+    prateleira: [],
+
+    adicionarlivro(nome, autor, categoria, estoque) {
+        const livro = {
+            nome: nome,
+            autor: autor,
+            categoria: categoria,
+            estoque: estoque,
+            disponivel: true,
+            alugado: false,
+            vezesalugado: 0
+        };
+
+        this.prateleira.push(livro);
+        console.log("Livro adicionado:", livro.nome);
+    },
+
+    mostrarprodutos() {
+        for (let i = 0; i < this.prateleira.length; i++) {
+            const livro = this.prateleira[i];
+            console.log(livro);
+        }
     }
+};
+
+biblioteca.adicionarlivro(
+    "O Livro",
+    "Larissa",
+    "Romance",
+    8
+);
+
+biblioteca.mostrarprodutos();
